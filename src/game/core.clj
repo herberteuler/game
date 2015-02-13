372ed7e9936ec95c9b82e8a19815d74740dfeddf
Guanpeng Xu <herberteuler@gmail.com>
2015-02-13 16:48:37 +0800

    Add measures profit, max-drawdown and streaks

--- src/game/core.clj
+++ src/game/core.clj
@@ -1,10 +1,34 @@
 (ns game.core
   (:gen-class))
 
-(def measure-fns [])
+(defn profit [m e e' env]
+  (let [x (- e' e)
+        {:keys [capital]} env]
+    {:net-profit x :%-gain (* (/ x capital) 100.0)}))
 
-(defn update-measures [measures e e']
-  (reduce (fn [m f] (merge m (f m e e'))) measures measure-fns))
+(defn max-drawdown [m e e' env]
+  (when (< e' e)
+    (let [x (- e' e)
+          {:keys [capital]} env
+          dd (* (/ x capital) 100.0)
+          {:keys [max-drawdown] :or {max-drawdown 0}} m]
+      (when (< dd max-drawdown)
+        {:max-drawdown dd}))))
+
+(defn streak [cmp stk max-stk]
+  (fn [m e e' env]
+    (if (cmp e' e)
+      {stk (inc (get m stk 0))}
+      {stk 0 max-stk (max (get m stk) (get m max-stk 0))})))
+
+(def winning-streak (streak > :winning-streak :longest-winning-streak))
+
+(def losing-streak (streak < :losing-streak :longest-losing-streak))
+
+(def measure-fns [profit max-drawdown winning-streak losing-streak])
+
+(defn update-measures [measures e e' env]
+  (reduce (fn [m f] (merge m (f m e e' env))) measures measure-fns))
 
 (defn draw [bag capital equity alloc]
   (let [amount (alloc capital equity), payoff (rand-nth capital)]
@@ -15,7 +39,7 @@
         equity' (draw bag capital equity alloc)]
     (assoc env
       :equity equity' :history (conj history equity')
-      :measures (update-measures measures equity equity'))))
+      :measures (update-measures measures equity equity' env))))
 
 (defn run [& args]
   (let [{:keys [bag capital alloc trial]} args]
