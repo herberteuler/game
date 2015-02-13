a30643dff3f726e96a24c53621a737bbcd90ce2a
Guanpeng Xu <herberteuler@gmail.com>
2015-02-13 09:56:39 +0800

    Initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject game "0.1"
+  :description "trading games"
+  :url ""
+  :license {:name "GNU GPL"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot game.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
